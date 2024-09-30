8a6fb1f44b3643faff455fd2f82ec65bb9db76a1
massimopallera <pallera.massimo@gmail.com>
2024-09-30 14:52:25 +0200

    docs: added comments

--- script.js
+++ script.js
@@ -7,22 +7,28 @@
 
 
 let priceX_Km = 0.21;
-let minor_discount = (20*priceX_Km)/100;
+
+//Calculated discounts here so it could be easier to change if the discount will be changed
+let minor_discount = (20*priceX_Km)/100; 
 let over65_discount = (40*priceX_Km)/100;
 let actual_price;
 
 
 let user_km = prompt("Inserire numero di km da percorrere:");
 
+//to control if the user inserts a valid number of kms
 if (user_km > 0){
     
     let age = prompt("Inserire eta`:")
     
+    //to control if user inserts a valid age  (used 100 as maximum age, it could be changed anyway)
     if (age > 0 && age < 100){
 
+        //calculate the total price 
         let total_price = priceX_Km*user_km
 
 
+        //control which discount has to be used
         if(age < 18){
             discount = (minor_discount * user_km)
         } else if (age >= 65){
@@ -31,13 +37,15 @@ if (user_km > 0){
             discount = 0
         }
 
+        // subtract discount from total price
         actual_price = (total_price-discount)
+
+        //round the decimal part
         let rounded = Math.round(actual_price * 100) / 100;
 
 
         let message = `Il costo per percorrere ${user_km}Km è di €${rounded}`;
         console.log(message);
-
     }
     else{
         console.log("Errore. L'età inserita non è valida")
